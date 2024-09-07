d09dea94edd24df54cc2c8d15a2f6b9282b044e2
Cheasley91 <w.c.heasley@gmail.com>
2024-09-07 13:11:49 -0500

    Added a black window with the ability to close out with x

--- main.py
+++ main.py
@@ -7,9 +7,25 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # Exit the game loop
+
+    # Fill the screen with black (RGB: 0, 0, 0)
+    screen.fill((0, 0, 0))
+
+    # Refresh the screen
+    pygame.display.flip()
+
+    
 if __name__ == "__main__":
     main()
